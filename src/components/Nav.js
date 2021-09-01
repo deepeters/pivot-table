1b2c27ba4260d4fd3c6217718a9b1133352f7349
Dennis Peters <dennis95peters@gmail.com>
2021-09-01 08:40:22 +0300

    Eliminate minor bugs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalculator } from '@fortawesome/free-solid-svg-icons'
 
-const Nav = () => {
-    return(
-        <nav>
-            <h1>Sales</h1>
-            <button>
-                Sum of Sales
-                <FontAwesomeIcon icon ={faCalculator} />
-            </button>
-        </nav>
-    )
+class Nav extends React.Component {
+    render() {
+        return(
+            <nav>
+                <h1>Sales</h1>
+                <button>
+                    Sum of Sales
+                    <FontAwesomeIcon icon ={faCalculator} />
+                </button>
+            </nav>
+        )
+    }
 };
 
 export default Nav; 
